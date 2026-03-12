8314889d090ff1c4446a05b1730ab185117263d2
Professor <Professor@SN-344671.edu.fiesc.com.br>
2026-03-12 12:02:16 -0300

    Atividade 01 Console

--- Aula01_Fundamentos/src/br/senai/ds2026/javafundamentos/aula01_console/CadastroProduto.java
+++ Aula01_Fundamentos/src/br/senai/ds2026/javafundamentos/aula01_console/CadastroProduto.java
@@ -1,6 +1,6 @@
 package br.senai.ds2026.javafundamentos.aula01_console;
 
-public class Exemplo01 {
+public class CadastroProduto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ Aula01_Fundamentos/src/br/senai/ds2026/javafundamentos/aula03_condicionais/Exemplo03ClassificacaoIdade.java
@@ -0,0 +1,10 @@
+package br.senai.ds2026.javafundamentos.aula03_condicionais;
+
+public class Exemplo03ClassificacaoIdade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
